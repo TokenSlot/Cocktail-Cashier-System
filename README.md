dfb0e950aabb535724c3437759469af1dce1ce23
TokenSlot <rompalt37@gmail.com>
2018-10-07 00:43:16 +0800

    Started on Remove Menu and Mix Menu

--- README.md
+++ README.md
@@ -5,6 +5,13 @@ Developed by [Paltera], [Paragas] and [Fonseca]
 
 ![cocktailImg](https://images.all-free-download.com/images/graphiclarge/cocktail_of_highdefinition_picture_five_166470.jpg)
 
+TODO:
+[X] Add Display of Items on Current Purchase Menu and Payment Menu
+[] Fix Prices
+[] Add Mix Menu
+[] Better Quantity Handling
+[] Finish Remove Menu
+
 [Paltera]: https://github.com/TokenSlot
 [Fonseca]: https://github.com/iashiarii
 [Paragas]: https://github.com/owel123
--- pradyek.cpp
+++ pradyek.cpp
@@ -10,8 +10,10 @@
 using namespace std;
 
 //Receipt Responsive Dashes
-//test
 void dasher(string str, float price);
+
+//For Remove Menu
+void removeMenu(int index, string str, float price);
 
 //Menu Responsive Dots
 void dotter(string str, float price);
@@ -59,20 +61,20 @@ main() {
 	vGin.push_back("Hearts on Fire");
 	
 	vector<float> vGinPrice;
-	vGinPrice.push_back(127.51);
-	vGinPrice.push_back(242.45);
-	vGinPrice.push_back(132.27);
-	vGinPrice.push_back(196.78);
-	vGinPrice.push_back(235.27);
-	vGinPrice.push_back(217.45);
-	vGinPrice.push_back(672.2);
-	vGinPrice.push_back(195.7);
-	vGinPrice.push_back(403.32);
-	vGinPrice.push_back(132.81);
-	vGinPrice.push_back(245.03);
-	vGinPrice.push_back(177.76);
-	vGinPrice.push_back(172.39);
-	vGinPrice.push_back(226.06);
+	vGinPrice.push_back(127.51); //Artillery Cocktail
+	vGinPrice.push_back(242.45); //Earthquake
+	vGinPrice.push_back(132.27); //Flying Dutchman
+	vGinPrice.push_back(196.78); //Gibson
+	vGinPrice.push_back(235.27); //Gin and it
+	vGinPrice.push_back(217.45); //Long Drink
+	vGinPrice.push_back(672.2); //St. Germain Martini
+	vGinPrice.push_back(195.7); //Barrel Choke
+	vGinPrice.push_back(403.32); //Caruso #1
+	vGinPrice.push_back(132.81); //Cafe de Paris Cocktail
+	vGinPrice.push_back(245.03); //Fifty-Fifty
+	vGinPrice.push_back(177.76); //Hula-hula
+	vGinPrice.push_back(172.39); //Fare Thee Well Martini
+	vGinPrice.push_back(226.06); //Hearts on Fire
 	
 	vector<string> vWhiskey;
 	vWhiskey.push_back("T.N.T");
@@ -91,20 +93,20 @@ main() {
 	vWhiskey.push_back("Pear Noel");
 	
 	vector<float> vWhiskeyPrice;
-	vWhiskeyPrice.push_back(227.51);
-	vWhiskeyPrice.push_back(330.52);
-	vWhiskeyPrice.push_back(235.40);
-	vWhiskeyPrice.push_back(375.75);
-	vWhiskeyPrice.push_back(350.20);
-	vWhiskeyPrice.push_back(299.99);
-	vWhiskeyPrice.push_back(201.90);
-	vWhiskeyPrice.push_back(221.11);
-	vWhiskeyPrice.push_back(323.45);
-	vWhiskeyPrice.push_back(243.66);
-	vWhiskeyPrice.push_back(250.50);
-	vWhiskeyPrice.push_back(242.44);
-	vWhiskeyPrice.push_back(344.47);
-	vWhiskeyPrice.push_back(369.69);
+	vWhiskeyPrice.push_back(227.51); //T.N.T
+	vWhiskeyPrice.push_back(330.52); //Barracuda
+	vWhiskeyPrice.push_back(235.40); //Black Forest Cake
+	vWhiskeyPrice.push_back(375.75); //Fire-wall
+	vWhiskeyPrice.push_back(350.20); //Full Metal Jacket
+	vWhiskeyPrice.push_back(299.99); //The Obama
+	vWhiskeyPrice.push_back(201.90); //The Heim Lick
+	vWhiskeyPrice.push_back(221.11); //Purgatory
+	vWhiskeyPrice.push_back(323.45); //The Three Amigos
+	vWhiskeyPrice.push_back(243.66); //Fatigue
+	vWhiskeyPrice.push_back(250.50); //Final Straw
+	vWhiskeyPrice.push_back(242.44); //I See Dead People
+	vWhiskeyPrice.push_back(344.47); //Prosecutor
+	vWhiskeyPrice.push_back(369.69); //Pear Noel
 	
 	vector<string> vVodka;
 	vVodka.push_back("Bullshot");
@@ -124,20 +126,21 @@ main() {
 	vVodka.push_back("Mother-in-law");
 	
 	vector<float> vVodkaPrice;
-	vVodkaPrice.push_back(127.56);
-	vVodkaPrice.push_back(130.52);
-	vVodkaPrice.push_back(135.40);
-	vVodkaPrice.push_back(175.75);
-	vVodkaPrice.push_back(150.32);
-	vVodkaPrice.push_back(199.99);
-	vVodkaPrice.push_back(101.90);
-	vVodkaPrice.push_back(111.11);
-	vVodkaPrice.push_back(123.45);
-	vVodkaPrice.push_back(143.66);
-	vVodkaPrice.push_back(150.50);
-	vVodkaPrice.push_back(142.54);
-	vVodkaPrice.push_back(144.47);
-	vVodkaPrice.push_back(143.69);
+	vVodkaPrice.push_back(127.56); //Bullshot
+	vVodkaPrice.push_back(130.52); //Appletini
+	vVodkaPrice.push_back(135.40); //Apple Bomb
+	vVodkaPrice.push_back(175.75); //Black Russian
+	vVodkaPrice.push_back(150.32); //Death Rattle
+	vVodkaPrice.push_back(199.99); //Hole in One
+	vVodkaPrice.push_back(101.90); //Kamikaze
+	vVodkaPrice.push_back(111.11); //Pukie Goop
+	vVodkaPrice.push_back(123.45); //Sun burn
+	vVodkaPrice.push_back(143.66); //Corkscrew
+	vVodkaPrice.push_back(150.50); //Cobra
+	vVodkaPrice.push_back(142.54); //Kiss of an angel
+	vVodkaPrice.push_back(144.47); //Mom's Milk
+	vVodkaPrice.push_back(143.69); //I see nothing
+	vVodkaPrice.push_back(143.69); //Mother-in-law
 	
 	vector<string> vRum;
 	vRum.push_back("Broken ankle");
@@ -156,20 +159,20 @@ main() {
 	vRum.push_back("Run, Run as fast as you can");
 	
 	vector<float> vRumPrice;
-	vRumPrice.push_back(127.51);
-	vRumPrice.push_back(130.52);
-	vRumPrice.push_back(135.40);
-	vRumPrice.push_back(175.75);
-	vRumPrice.push_back(150.21);
-	vRumPrice.push_back(199.99);
-	vRumPrice.push_back(101.90);
-	vRumPrice.push_back(111.11);
-	vRumPrice.push_back(123.45);
-	vRumPrice.push_back(143.66);
-	vRumPrice.push_back(150.50);
-	vRumPrice.push_back(142.65);
-	vRumPrice.push_back(144.47);
-	vRumPrice.push_back(143.69);
+	vRumPrice.push_back(127.51); //Broken ankle
+	vRumPrice.push_back(130.52); //Quicky
+	vRumPrice.push_back(135.40); //Belly
+	vRumPrice.push_back(175.75); //Dragon's Mouth
+	vRumPrice.push_back(150.21); //Davy Jones' Locker
+	vRumPrice.push_back(199.99); //Jaguar
+	vRumPrice.push_back(101.90); //Martinique
+	vRumPrice.push_back(111.11); //My Sharona
+	vRumPrice.push_back(123.45); //Midnight Mass
+	vRumPrice.push_back(143.66); //Suffering Bastard
+	vRumPrice.push_back(150.50); //Stroll Through The Woods
+	vRumPrice.push_back(142.65); //BeBe
+	vRumPrice.push_back(144.47); //You can't catch me
+	vRumPrice.push_back(143.69); //Run, Run as fast as you can
 	
 	vector<string> vTequilla;
 	vTequilla.push_back("BTC");
@@ -189,46 +192,66 @@ main() {
 	vTequilla.push_back("Uno, Dos, Tres");
 	
 	vector<float> vTequillaPrice;
-	vTequillaPrice.push_back(127.51);
-	vTequillaPrice.push_back(130.52);
-	vTequillaPrice.push_back(135.40);
-	vTequillaPrice.push_back(175.75);
-	vTequillaPrice.push_back(150.05);
-	vTequillaPrice.push_back(199.99);
-	vTequillaPrice.push_back(101.90);
-	vTequillaPrice.push_back(111.11);
-	vTequillaPrice.push_back(123.45);
-	vTequillaPrice.push_back(143.66);
-	vTequillaPrice.push_back(150.50);
-	vTequillaPrice.push_back(142.99);
-	vTequillaPrice.push_back(144.47);
-	vTequillaPrice.push_back(143.69);
-	vTequillaPrice.push_back(144.47);
+	vTequillaPrice.push_back(127.51); //BTC
+	vTequillaPrice.push_back(130.52); //Black Cat
+	vTequillaPrice.push_back(135.40); //The Raging Bull
+	vTequillaPrice.push_back(175.75); //Easy Tiger
+	vTequillaPrice.push_back(150.05); //Hign Noon
+	vTequillaPrice.push_back(199.99); //Paloma
+	vTequillaPrice.push_back(101.90); //Sweet Dreams
+	vTequillaPrice.push_back(111.11); //Del Rio
+	vTequillaPrice.push_back(123.45); //East of Eden
+	vTequillaPrice.push_back(143.66); //EL Santo
+	vTequillaPrice.push_back(150.50); //Siesta
+	vTequillaPrice.push_back(142.99); //Untitled
+	vTequillaPrice.push_back(144.47); //Coup De Grace
+	vTequillaPrice.push_back(143.69); //Rio Grande
+	vTequillaPrice.push_back(144.47); //Uno, Dos, Tres
 	
-//	vector<string> vMix;
-//	vMix.push_back("Egg");
-//	vMix.push_back("Beef Broth");
-//	vMix.push_back("Hot Sauce");
-//	vMix.push_back("Beer");
-//	vMix.push_back("Maggi Sauce");
-//	vMix.push_back("Moonshine");
-//	vMix.push_back("Orange Juice");
-//	vMix.push_back("Limes");
-//	vMix.push_back("Squid Ink");
-//	vMix.push_back("Brandy");
-//	vMix.push_back("Avocado");
-//	vMix.push_back("Matcha Powder");
-//	vMix.push_back("Lemon");
-//	vMix.push_back("Coffee");
-//	vMix.push_back("Apple cider");
-//	vMix.push_back("Pineapple juice");
-//	vMix.push_back("Iced tea");
-//	vMix.push_back("Watermelon");
+	vector<string> vMix;
+	vMix.push_back("Egg");
+	vMix.push_back("Beef Broth");
+	vMix.push_back("Hot Sauce");
+	vMix.push_back("Beer");
+	vMix.push_back("Maggi Sauce");
+	vMix.push_back("Moonshine");
+	vMix.push_back("Orange Juice");
+	vMix.push_back("Limes");
+	vMix.push_back("Squid Ink");
+	vMix.push_back("Brandy");
+	vMix.push_back("Avocado");
+	vMix.push_back("Matcha Powder");
+	vMix.push_back("Lemon");
+	vMix.push_back("Coffee");
+	vMix.push_back("Apple cider");
+	vMix.push_back("Pineapple juice");
+	vMix.push_back("Iced tea");
+	vMix.push_back("Watermelon");
 	
-
-	int input = 3, index, choose;
+	vector<float> vMixPrice;
+	vMixPrice.push_back(6); //egg
+	vMixPrice.push_back(35); //Beef Broth
+	vMixPrice.push_back(25); //Hot Sauce
+	vMixPrice.push_back(50); //Beer
+	vMixPrice.push_back(15); //Maggi Sauce
+	vMixPrice.push_back(55); //Moonshine
+	vMixPrice.push_back(12); //Orange Juice
+	vMixPrice.push_back(20); //Limes
+	vMixPrice.push_back(25); //Squid Ink
+	vMixPrice.push_back(55); //Brandy
+	vMixPrice.push_back(31); //Avocado
+	vMixPrice.push_back(18); //Matcha Powder
+	vMixPrice.push_back(22); //Lemon
+	vMixPrice.push_back(11); //Coffee
+	vMixPrice.push_back(12.75); //Apple cide
+	vMixPrice.push_back(17); //Pineapple juice
+	vMixPrice.push_back(20); //Iced Tea
+	vMixPrice.push_back(30); //Watermelon
+	
+	//Variables
+	int input = 3, index, choose, remove;
 	int i, myRand;
-	float price, total = 0, payment, change;
+	float price, tempTotal = 0, total = 0, payment, change;
 	char another, upper = 'Y';
 	string main;
 	
@@ -244,8 +267,8 @@ main() {
 			cout<<"[4] Rum"<<endl;
 			cout<<"[5] Tequilla"<<endl;
 			//cout<<"[6] Mix your own"<<endl<<endl; Not yet done!
-			if (input < 0 || input> 5) {
-				cout<<"Enter 0 to 5 only"<<endl;
+			if (input < 0 || input> 6) {
+				cout<<"Enter 0 to 6 only"<<endl;
 			}
 			cout<<"Input: ";
 			cin>>input;
@@ -257,45 +280,100 @@ main() {
 				case 3: main = "Vodka"; break;
 				case 4: main = "Rum"; break;
 				case 5: main = "Tequilla"; break;
+				case 6: main = "Mix"; break;
 			}
 			system("cls");
 			
 			cout<<"[0] Back"<<endl;
 			if (main == "Gin") {
-
-				choose = getList(choose,vGin,vGinPrice,vChoice, vPrice);
+				choose = getList(choose, vGin, vGinPrice, vChoice, vPrice);
 			} else if (main == "Whiskey") {
-
-				choose = getList(choose,vWhiskey,vWhiskeyPrice,vChoice, vPrice);
+				choose = getList(choose, vWhiskey, vWhiskeyPrice, vChoice, vPrice);
 			} else if (main == "Vodka") {
-
-				choose = getList(choose,vVodka,vVodkaPrice,vChoice, vPrice);
+				choose = getList(choose, vVodka, vVodkaPrice, vChoice, vPrice);
 			} else if (main == "Rum") {
-
-				choose = getList(choose,vRum,vRumPrice,vChoice, vPrice);
+				choose = getList(choose, vRum, vRumPrice, vChoice, vPrice);
 			} else if (main == "Tequilla") {
-
-				choose = getList(choose,vTequilla,vTequillaPrice,vChoice, vPrice);
+				choose = getList(choose, vTequilla, vTequillaPrice, vChoice, vPrice);
+			} else {
+				choose = getList(choose, vMix, vMixPrice, vChoice, vPrice);
 			}
 			system("cls");
-		} while(input < 1 || input > 5 || choose== 0);
+		} while(input < 1 || input > 6 || choose== 0);
 		
-		cout<<"[Y] Buy Another"<<endl;
-		cout<<"[N] Check Out"<<endl;
-		cout<<"[C] Cancel"<<endl; 
-		cout<<endl<<"Press other keys to go back."<<endl;
-		cout<<"Input: ";
-		cin>>another;
-		upper = toupper(another);
-		if (upper == 'Y' || upper == 'N') {
-			total += vPrice.back();
-		} else if (upper == 'C') {
-			cout<<endl<<"--Transaction Cancelled--"; 
-			exit(0);
-		} else {
-			upper = 'Y';
-		}
-		system("cls");
+		do {
+			if (!vChoice.empty()) {
+				cout<<"Current Purchases:"<<endl;
+				for (index = 0; index < vChoice.size(); index++) {
+					dasher(vChoice[index], vPrice[index]);
+					tempTotal += vPrice[index];
+				}
+				cout<<"\nTotal: "<<tempTotal;
+				cout<<endl<<endl;
+			}
+			cout<<"[Y] Buy Another"<<endl;
+			cout<<"[N] Check Out"<<endl;
+			cout<<"[R] Remove Purchase"<<endl;
+			cout<<"[C] Cancel"<<endl; 
+			cout<<endl<<"Press other keys to go back."<<endl;
+			cout<<"Input: ";
+			cin>>another;
+			upper = toupper(another);
+			if (upper == 'Y' || upper == 'N') {
+				cout<<"\nAre you sure? "<<endl;
+				cout<<"[O] Yes"<<endl;
+				cout<<"[X] No"<<endl;
+				cout<<"Input: ";
+				cin>>another;
+				upper = toupper(another);
+				if (upper == 'O') {
+					total += vPrice.back();
+					upper = 'Y';
+				}
+			} else if (upper == 'C') {
+				cout<<endl<<"--Transaction Cancelled--"; 
+				exit(0);
+			} else if (upper == 'R') {
+				do {
+					system("cls");
+					if (vChoice.size() == 1) {
+						cout<<"Are you sure you want to remove "<<vChoice[0]<<"?"<<endl;
+						cout<<"[O] Yes"<<endl;
+						cout<<"[X] No"<<endl;
+						cout<<"Input: ";
+						cin>>another;
+						upper = toupper(another);
+						if (upper == 'O') {
+							vChoice.erase(vChoice.begin());
+						}
+						upper = 'Y';
+					} else if (vChoice.size() > 1) {
+						remove = 1;
+						do {
+							if (remove < 0 || remove > vChoice.size()) {
+								cout<<"Enter 1 to "<<vChoice.size()<<" only"<<endl;
+							}
+ 							cout<<"Choose item to remove: ";
+							for (index = 0; index < vChoice.size(); index++) {
+								removeMenu(index + 1, vChoice[index], vPrice[index]);
+							}
+							cout<<"\nTotal: "<<total;
+							cout<<endl;
+							cout<<"Input: ";
+							cin>>remove;
+						} while (remove < 0 || remove > vChoice.size());
+					} else {
+						cout<<"There are no items to remove..."<<endl;
+						cout<<"Press any button to continue.";
+						cin.get();
+						upper = 'Y';
+					}
+				} while (upper == 'O');
+			} else {
+				upper = 'Y';
+			}
+			system("cls");
+		} while (upper == 'X');
 	} while(upper == 'Y');
 	
 	payment = total + 1;
@@ -313,6 +391,11 @@ main() {
 			}
 			
 		}
+		cout<<"Purchases:"<<endl;
+		for (index = 0; index < vChoice.size(); index++) {
+			dasher(vChoice[index], vPrice[index]);
+		}
+		cout<<endl;
 		cout<<"Total: "<<total<<endl;
 		cout<<"Payment: ";
 		cin>>payment;
@@ -363,6 +446,14 @@ void dasher(string str, float price) {
 	cout<<strPrice<<endl;
 }
 
+//For Remove Menu
+void removeMenu(int index, string str, float price) {
+	string strPrice;
+	strPrice = "P" + tostr(price);
+	cout<<"["<<index<<"] "<<str;
+	cout<<" - "<<strPrice<<endl;
+}
+
 //Menu Responsive Dots
 void dotter(string str, float price) {
 	string strPrice;
@@ -379,7 +470,7 @@ void dotter(string str, float price) {
 
 //Returns Choose (x) and Displays List of chosen main ingredient
 int getList(int choose, vector<string> &itemList, vector<float> &itemPrice, vector<string> &itemChoice, vector<float> &itemChoicePrice){
-	int index, x;
+	int index, x, q = 1;
 	for (index = 0; index < itemList.size(); index++) {
 		cout<<"["<<index+1<<"] ";
 		dotter(itemList[index], itemPrice[index]);
@@ -388,14 +479,24 @@ int getList(int choose, vector<string> &itemList, vector<float> &itemPrice, vect
 	cout<<"Enter the Number of your choice: ";
 	x = getChoose(choose);
 	if (x != 0) {
+		do {
+			if (q == 0) {
+				cout<<"You can't enter zero (0) as a quantity"<<endl;
+			}
+			cout<<"Enter quantity: ";
+			cin>>q;
+			if (q == 0) {
+				system("cls");
+			}
+		} while (q == 0);
 		itemChoice.push_back(itemList[x-1]);
-		itemChoicePrice.push_back(itemPrice[x-1]);
+		itemChoicePrice.push_back(itemPrice[x-1]*q);
 	}
 	return x;
 }
 
 //Input Choose (for getlist function)
-int getChoose(int &choose){
+int getChoose(int &choose) {
 	cin >> choose;
 	if (cin.fail()) {
 		cin.clear();
