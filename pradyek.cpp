c25cb2659b26b51496fcee29ff7bd358b074e556
TokenSlot <rompalt37@gmail.com>
2018-10-12 07:17:53 +0800

    getch

--- pradyek.cpp
+++ pradyek.cpp
@@ -219,50 +219,11 @@ main() {
 	vTequillaPrice.push_back(144.47); //Coup De Grace
 	vTequillaPrice.push_back(143.69); //Rio Grande
 	vTequillaPrice.push_back(144.47); //Uno, Dos, Tres
-	
-	vector<string> vMix;
-	vMix.push_back("Egg");
-	vMix.push_back("Beef Broth");
-	vMix.push_back("Hot Sauce");
-	vMix.push_back("Beer");
-	vMix.push_back("Maggi Sauce");
-	vMix.push_back("Moonshine");
-	vMix.push_back("Orange Juice");
-	vMix.push_back("Limes");
-	vMix.push_back("Squid Ink");
-	vMix.push_back("Brandy");
-	vMix.push_back("Avocado");
-	vMix.push_back("Matcha Powder");
-	vMix.push_back("Lemon");
-	vMix.push_back("Coffee");
-	vMix.push_back("Apple cider");
-	vMix.push_back("Pineapple juice");
-	vMix.push_back("Iced tea");
-	vMix.push_back("Watermelon");
-	
-	vector<float> vMixPrice;
-	vMixPrice.push_back(6); //Egg
-	vMixPrice.push_back(50); //Beef Broth
-	vMixPrice.push_back(25); //Hot Sauce
-	vMixPrice.push_back(50); //Beer
-	vMixPrice.push_back(15); //Maggi Sauce
-	vMixPrice.push_back(55); //Moonshine
-	vMixPrice.push_back(12); //Orange Juice
-	vMixPrice.push_back(20); //Limes
-	vMixPrice.push_back(25); //Squid Ink
-	vMixPrice.push_back(55); //Brandy
-	vMixPrice.push_back(31); //Avocado
-	vMixPrice.push_back(18); //Matcha Powder
-	vMixPrice.push_back(22); //Lemon
-	vMixPrice.push_back(11); //Coffee
-	vMixPrice.push_back(12.75); //Apple cide
-	vMixPrice.push_back(17); //Pineapple juice
-	vMixPrice.push_back(20); //Iced Tea
-	vMixPrice.push_back(30); //Watermelon
+
 	
 	//Declaration of Variables
 	int input = 3, userInput = 1, index, clearMix = 0, loop = 0 , choose, remove = 1, removeQuantity = 1, removePrice = 0, temp, mixCount = 0;
-	int i, myRand;
+	int i, myRand, q;
 	float price, tempTotal = 0, total = 0, payment, change, mainMix, mixPrice = 0, mixTempPrice = 0;
 	char another, check, upper = 'Y', userConfirm;
 	string main, mixName;
@@ -276,7 +237,7 @@ main() {
 				system("cls");
 				cout<<"----- Welcome to the Space Bar -----"<<endl;
 				cout<<endl;
-				cout<<"Choose your drink"<<endl;
+				cout<<" --- Main Menu ---"<<endl<<endl;
 				cout<<"[0] Exit"<<endl;
 				cout<<" --------------------- "<<endl;
 				for (index = 0; index < 5; index++) {
@@ -300,7 +261,7 @@ main() {
 				case 3: main = "Vodka"; break;
 				case 4: main = "Rum"; break;
 				case 5: main = "Tequilla"; break;
-				case 6: main = "Mix"; break;
+
 			}
 			system("cls");
 			if (main == "Gin") {
@@ -313,83 +274,6 @@ main() {
 				choose = getList(main, choose, vRum, vRumPrice, vChoice, vPrice, vMul);
 			} else if (main == "Tequilla") {
 				choose = getList(main, choose, vTequilla, vTequillaPrice, vChoice, vPrice, vMul);
-			} else if (main == "Mix") {
-				//MIX MENU
-				clearMix = 0;
-				do {
-					do {
-						mixTempPrice = 0;
-						if (clearMix = 1) {
-							vMixer.clear();
-							vMixerPrice.clear();
-							vMixerMul.clear();
-						}
-						system("cls");
-						cout<<"----- Mix Menu -----"<<endl;
-						cout<<endl;
-						cout<<"Choose the main ingredient of your Drink"<<endl;
-						cout<<"[0] Back"<<endl;
-						cout<<" --------------------- "<<endl;
-						cout<<"[1] Gin - P56.21"<<endl;
-						cout<<"[2] Whiskey - P52.97"<<endl;
-						cout<<"[3] Vodka - P42.16"<<endl;
-						cout<<"[4] Rum - P40.00"<<endl;
-						cout<<"[5] Tequilla - P48.10"<<endl<<endl;
-						if (userInput < 0 || userInput > 5) {
-							cout<<"Enter 0 to 5 only"<<endl;
-						}
-						cout<<"Input: ";
-						cin>>userInput;
-						mixTempPrice = mainPrices[userInput-1];
-						if (cin.fail()) {
-							input = -1;
-						}
-						clearInput();
-					} while (userInput < 0 || userInput > 5);
-					
-					if (userInput != 0) {
-						do {
-							system("cls");
-							cout<<"Choose the other ingredient of your Drink"<<endl;
-							cout<<"[0] Back"<<endl;
-							cout<<" --------------------- "<<endl;
-							for (index = 0; index < vMix.size(); index++) {
-								cout<<"["<<index+1<<"] "<<vMix[index]<<" - P"<<vMixPrice[index]<<endl;
-							}
-							cout<<" --------------------- "<<endl;
-							cout<<"["<<vMix.size()+1<<"] Remove Ingredient"<<endl<<endl;
-							if (userInput < 1 || userInput >= vMix.size()) cout<<"Invalid Input"<<endl;
-							cout<<"Input: ";
-							cin>>userInput;
-							if (cin.fail()) {
-								userInput = -1;
-							} else if (userInput == 0) {
-								do {
-									clearInput();
-									cout<<"Are you sure you want to go back?"<<endl;
-									cout<<"This will clear everything in the Mixer"<<endl;
-									cout<<"[O] Yes"<<endl;
-									cout<<"[X] No"<<endl<<endl;
-									if (toupper(userConfirm) != 'O' && toupper(userConfirm) != 'X') {
-										cout<<"Invalid Input"<<endl;
-									}
-									cout<<"Input: ";
-									cin>>userConfirm;
-								} while (toupper(userConfirm) != 'O' && toupper(userConfirm) != 'X');
-								if (toupper(userConfirm) == 'O') {
-									loop = 1;
-								} else {
-									userInput = -1;
-								}
-							} else if (userInput >= 1 && userInput <= vMix.size()+1) {
-								
-							}
-							clearInput();
-						} while (userInput < 1 || userInput >= vMix.size());
-					} else {
-						choose = 0;
-					} 
-				} while (loop == 1);
 			}
 			system("cls");
 		} while(choose == 0);
